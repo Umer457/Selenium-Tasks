9093b129ade570d307ebd0fc50d4fd77ad260bed
Umer457 <68316446+Umer457@users.noreply.github.com>
2023-01-27 10:22:24 +0500

    Made changes mentioned in the feedback

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -16,59 +16,69 @@ class Methods:
         driver.maximize_window()
         driver.get(base_url)
         driver.implicitly_wait(10)
+
+
+    def google_navigation(self):
+
         search_bar = driver.find_element(By.XPATH, Xpaths.xpath_search_bar)
         search_bar.send_keys("Tintash")
-        time.sleep(1)
         search_bar.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-    def secondStep(self):
+
+
+
+    def tintash_navigation(self):
 
         web_link = driver.find_element(By.XPATH, Xpaths.xpath_tintash_link)
         web_link.click()
-        time.sleep(5)
 
         port_link = driver.find_element(By.XPATH, Xpaths.xpath_portfolio)
         port_link.click()
-        time.sleep(3)
-
-    def thirdStep(self):
+        driver.implicitly_wait(2)
 
         scroll_to_tabs = driver.find_element(By.XPATH, Xpaths.xpath_tabs_view)
         driver.execute_script("arguments[0].scrollIntoView()", scroll_to_tabs)
-        time.sleep(1)
+        time.sleep(2)
 
         solution_menu = driver.find_element(By.XPATH, Xpaths.xpath_solution_tab)
         solution_menu.click()
-        time.sleep(1)
+
 
         first_option = driver.find_element(By.XPATH, Xpaths.xpath_solution_option)
         first_option.click()
-        time.sleep(1)
+
+
 
         technology_menu = driver.find_element(By.XPATH, Xpaths.xpath_technology_tab)
         technology_menu.click()
 
+
         second_option = driver.find_element(By.XPATH, Xpaths.xpath_technology_option)
         second_option.click()
-        time.sleep(1)
+
+
 
         industry_menu = driver.find_element(By.XPATH, Xpaths.xpath_industry_tab)
         industry_menu.click()
 
+
         third_option = driver.find_element(By.XPATH, Xpaths.xpath_industry_option)
         driver.execute_script("arguments[0].click();", third_option)
-        time.sleep(1)
+
+
 
         btn_search = driver.find_element(By.XPATH, Xpaths.xpath_search_button)
         btn_search.click()
-        time.sleep(1)
+
 
         wait = WebDriverWait(driver, 10)
         link_camaradly = wait.until(EC.presence_of_element_located((By.XPATH, Xpaths.xpath_camaradly)))
+        assert link_camaradly
         link_camaradly.location_once_scrolled_into_view
-        self.assertTrue = link_camaradly
-        time.sleep(5)
+
+
+
+
 
 
     def tearDown(self):
@@ -77,6 +87,6 @@ class Methods:
 
 function_call = Methods()
 function_call.setup()
-function_call.secondStep()
-function_call.thirdStep()
+function_call.google_navigation()
+function_call.tintash_navigation()
 function_call.tearDown()
--- Assignment3/test_assignment3.py
+++ Assignment3/test_assignment3.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 import time
-from Assignment3Xpaths import xpaths
+from PIL import Image
+from test_assignment3Xpaths import xpaths
 
+class Test_DragAndDrop:
 
-class DragAndDrop:
-    def setup(self):
+    def test_setup(self):
         global driver
         base_url = "https://jqueryui.com/droppable/"
         driver = webdriver.Firefox()
@@ -14,18 +15,36 @@ class DragAndDrop:
         driver.get(base_url)
         driver.implicitly_wait(10)
 
-    def dragDrop(self):
+
+
+    def test_dragDrop(self):
+
         driver.switch_to.frame(driver.find_element(By.XPATH, xpaths.xpath_frame_switch))
         drag_element = driver.find_element(By.XPATH, xpaths.xpath_drag_element)
         drop_element = driver.find_element(By.XPATH, xpaths.xpath_drop_element)
         ActionChains(driver).drag_and_drop(drag_element, drop_element).perform()
         time.sleep(5)
 
-    def tearDown(self):
+
+    def test_failiure(self):
+
+        btn_sample = driver.find_element(By.XPATH, "/html/body/header/div/nav/ol/li[4]/a")
+        if btn_sample:
+            print("exists")
+        else:
+            assert btn_sample
+
+
+
+
+    def test_tearDown(self):
+        driver.get_screenshot_as_file("screenshot.png")
+        screenshot = Image.open("screenshot.png")
+        screenshot.show()
         driver.quit()
 
 
-drag_drop_call = DragAndDrop()
-drag_drop_call.setup()
-drag_drop_call.dragDrop()
-drag_drop_call.tearDown()
+drag_drop_call = Test_DragAndDrop()
+drag_drop_call.test_setup()
+drag_drop_call.test_dragDrop()
+drag_drop_call.test_tearDown()
